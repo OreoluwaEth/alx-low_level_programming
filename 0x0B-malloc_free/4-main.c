60a3bfa6a0f42835c961ae54603cc30e09577791
Oreoluwa Adepegba <101391928+OreoluwaEth@users.noreply.github.com>
2022-05-05 22:08:51 +0100

    free grid

--- 0x0B-malloc_free/4-main.c
+++ 0x0B-malloc_free/4-main.c
@@ -1,18 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* print_grid - prints a grid of integers
-* @grid: the address of the two dimensional grid
-* @width: width of the grid
-* @height: height of the grid
-*
-* Return: Nothing.
-*/
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
 void print_grid(int **grid, int width, int height)
 {
 int w;
 int h;
+
 h = 0;
 while (h < height)
 {
@@ -24,16 +26,18 @@ w++;
 }
 printf("\n");
 h++;
+}   
 }
-}
+
 /**
-* main - check the code for ALX School students.
-*
-* Return: Always 0.
-*/
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 int **grid;
+
 grid = alloc_grid(6, 4);
 if (grid == NULL)
 {
